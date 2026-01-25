73b506b6c19b2b7f53a8e8a88d6908507edc113f
Jorge Borrero <jorge@example.com>
2026-01-25 15:37:51 -0500

    otros cambios  nuevos

--- pages/api/guardar.js
+++ pages/api/guardar.js
@@ -1,4 +1,4 @@
-const { appendRow } = require('../../lib/hojasGoogle');
+const { appendRow } = require('../../lib/googleSheets.js');
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
--- pages/api/listado.js
+++ pages/api/listado.js
@@ -1,4 +1,4 @@
-const { getRows } = require('../../lib/googleSheets');
+const { getRows } = require('../../lib/googleSheets.js');
 
 export default async function handler(req, res) {
   if (req.method !== 'GET') return res.status(405).json({ error: 'Method not allowed' });
